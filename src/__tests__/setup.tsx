5174c3ae7327305c0f4ea67017e3aa089d0f7ee3
Andrei Yurkouski <emp74ark@gmail.com>
2025-03-24 20:06:35 +0100

    refactor: remove comments

--- src/__tests__/setup.tsx
+++ src/__tests__/setup.tsx
@@ -1,12 +1,10 @@
 import { vi } from 'vitest';
 
-// Next navigation hooks mock
 vi.mock('next/navigation', () => ({
   useRouter: vi.fn(),
   useSearchParams: vi.fn(),
 }));
 
-// Next Image mock
 vi.mock('next/image', () => ({
   default: (props: { src: string; alt: string; [key: string]: unknown }) => {
     const { src, alt, ...rest } = props;
