b361f17b5edbaef9309dbc3076aaca6ab77dd1df
romanwebdev101 <romanwebdev101@gmail.com>
2025-03-26 19:40:17 +0200

    fix: resolve merge confilcts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,15 @@
-import Link from 'next/link';
-import styles from './page.module.scss';
 import { Translated } from '@/components';
+import styles from './page.module.scss';
 
 export default function RootPage() {
   return (
     <div className={styles.test}>
-<<<<<<< HEAD
-      <h1>Empty app</h1>
-      <Link href="/rest-client/GET">REST Client</Link>
-=======
       <h1>
         <Translated scope="root" text="title" />
       </h1>
       <p>
         <Translated scope="root" text="description" />
       </p>
->>>>>>> 779e9d010b17ead4078f0ddd52b5d32eb219d904
     </div>
   );
 }
