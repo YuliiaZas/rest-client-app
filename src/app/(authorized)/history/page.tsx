f8d2c9b885ff60e3646a61833d5ed8f2d027ff85
romanwebdev101 <romanwebdev101@gmail.com>
2025-03-30 10:47:39 +0300

    feat: redirect to client with all params

--- src/app/(authorized)/history/page.tsx
+++ src/app/(authorized)/history/page.tsx
@@ -2,21 +2,46 @@
 
 import { useLocalStorage } from '@/hooks';
 import { IHistory } from '@/types';
+import { getSearchParams, getUpdatedUrl } from '@/utils';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import styles from './history.module.scss';
 
 export default function History() {
+  const router = useRouter();
   const [history] = useLocalStorage<IHistory[]>({
     key: 'history',
     defaultValue: [],
   });
+
+  const openClientWithParams = (params: IHistory) => {
+    const searchParams = getSearchParams(params.headers);
+    const updatedUrl = getUpdatedUrl(
+      params.method,
+      params.url,
+      params.body,
+      searchParams
+    );
+
+    router.push(`${process.env.APP_URL}${updatedUrl}`);
+  };
 
   return (
     <div className={styles.history}>
       <h1 className={styles.history__title}>History</h1>
 
       {history.length ? (
-        <p>History data</p>
+        <>
+          {history.map((params) => (
+            <button
+              type="button"
+              onClick={() => openClientWithParams(params)}
+              key={params.date}
+            >
+              {params.url}
+            </button>
+          ))}
+        </>
       ) : (
         <>
           <div className={styles.history__empty}>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,7 @@ export { decodeBase64 } from './decode-base64';
 export { encodeBase64 } from './encode-base64';
 export { formatHeaders } from './format-headers';
 export { getSearchParams } from './get-search-params';
+export { getUpdatedUrl } from './get-updated-url';
 export { isValidURL } from './is-valid-url';
 export { updateUrl } from './update-url';
 export * from './variable-utils';
