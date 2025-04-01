f2127f09fcba99dd357ec85a02fbb39c0bbd4dc0
YuliiaZas <julia.kv101@gmail.com>
2025-04-01 16:57:25 -0400

    feat: replace `Select` component with `Dropdown` component

--- src/components/locale-switcher/locale-switcher.tsx
+++ src/components/locale-switcher/locale-switcher.tsx
@@ -3,7 +3,8 @@
 import { useLocale, useTranslations } from 'next-intl';
 import { Locale, locales } from '@/i18n';
 import { setUserLocale } from '@/services/locale';
-import { Select } from '@/components';
+import { Dropdown, Icon } from '@/components';
+import styles from './locale-switcher.module.scss';
 
 export function LocaleSwitcher() {
   const locale = useLocale();
@@ -15,13 +16,20 @@ export function LocaleSwitcher() {
   }
 
   return (
-    <Select
-      defaultValue={locale}
-      action={onLocaleChange}
-      options={locales.map((el) => ({ label: el.toUpperCase(), value: el }))}
-      icon="translate"
-      disabled={true}
-      title={t('language')}
-    />
+    <div className={styles.container}>
+      <Icon iconName="translate" size="1.2rem" />
+      <Dropdown
+        items={locales.map((el) => ({ label: el.toUpperCase(), value: el }))}
+        selectedItem={locale}
+        showButtonBorder={false}
+        colors="dark"
+        dropdownClass={styles.select}
+        positionedRigth={true}
+        selectOption={onLocaleChange}
+        buttonChildren={
+          <span title={t('language')}>{locale.toUpperCase()}</span>
+        }
+      />
+    </div>
   );
 }
