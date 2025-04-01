c8d729a721e0a74ec4449420a60b9c57df85e2f3
Andrei Yurkouski <emp74ark@gmail.com>
2025-04-01 13:59:35 +0200

    feat: extract views to dynamic imports

--- src/app/(authorized)/variables/page.tsx
+++ src/app/(authorized)/variables/page.tsx
@@ -1,98 +1,10 @@
-'use client';
+import dynamic from 'next/dynamic';
+import { Spinner } from '@/components';
 
-import { Button, Column, Spinner, Table } from '@/components';
-import { useLocalStorage } from '@/hooks';
-import { IVariable } from '@/types';
-import { useEffect, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import styles from './variables.module.scss';
+const DynamicVariables = dynamic(() => import('@/views/variables'), {
+  loading: () => <Spinner />,
+});
 
 export default function Variables() {
-  const [newVariable, setNewVariable] = useState({
-    id: uuidv4(),
-    name: '',
-    value: '',
-  });
-  const [isLoading, setIsLoading] = useState(true);
-  const [variables, setVariables] = useLocalStorage<IVariable[]>({
-    key: 'variables',
-    defaultValue: [],
-  });
-
-  useEffect(() => {
-    setIsLoading(false);
-  }, [variables]);
-
-  const addVariable = () => {
-    if (newVariable.name && newVariable.value) {
-      setVariables([...variables, newVariable]);
-      setNewVariable({ id: uuidv4(), name: '', value: '' });
-    }
-  };
-
-  const deleteVariable = (id: string) => {
-    const filteredVariables = variables.filter(
-      (variable) => variable.id !== id
-    );
-    setVariables(filteredVariables);
-  };
-
-  return (
-    <div className={styles.variables}>
-      <h1 className={styles.variables__title}>Variables</h1>
-
-      {isLoading ? (
-        <div className={styles.wrapper}>
-          <Spinner />
-        </div>
-      ) : (
-        <Table data={variables} hasFooter={true}>
-          <Column
-            title="Variable Name"
-            type="data"
-            body={(data: IVariable) => <span>{data.name}</span>}
-            footer={
-              <input
-                placeholder="Variable Name"
-                value={newVariable.name}
-                onChange={(e) =>
-                  setNewVariable((prev: IVariable) => ({
-                    ...prev,
-                    name: e.target.value,
-                  }))
-                }
-                className={styles.input}
-              />
-            }
-          />
-          <Column
-            title="Variable Value"
-            type="data"
-            body={(data: IVariable) => <span>{data.value}</span>}
-            footer={
-              <input
-                placeholder="Variable Value"
-                value={newVariable.value}
-                onChange={(e) =>
-                  setNewVariable((prev: IVariable) => ({
-                    ...prev,
-                    value: e.target.value,
-                  }))
-                }
-                className={styles.input}
-              />
-            }
-          />
-          <Column
-            title="Actions"
-            type="actions"
-            body={(data: IVariable) => (
-              <Button onClick={() => deleteVariable(data.id)} text="Delete" />
-            )}
-            footer={<Button onClick={addVariable} text="Add" />}
-          />
-        </Table>
-      )}
-    </div>
-  );
+  return <DynamicVariables />;
 }
