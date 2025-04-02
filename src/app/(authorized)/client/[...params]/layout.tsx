a95e7f0a8dcc44153e7cb857d41f77c34654c8fe
romanwebdev101 <romanwebdev101@gmail.com>
2025-04-02 11:05:11 +0300

    fix: check correct method in client route

---
+++ src/app/(authorized)/client/[...params]/layout.tsx
@@ -0,0 +1,22 @@
+import { httpMethods, Method } from '@/data';
+import { redirect } from 'next/navigation';
+import { ReactNode } from 'react';
+
+type ClientWithParamsLayoutProps = {
+  children: ReactNode;
+  params: Promise<{ params: string[] }>;
+};
+
+export default async function ClientWithParamsLayout({
+  children,
+  params,
+}: ClientWithParamsLayoutProps) {
+  const method = (await params).params[0];
+  const isMethodCorrect = httpMethods.includes(method.toUpperCase() as Method);
+
+  if (!isMethodCorrect) {
+    redirect('/client/GET');
+  }
+
+  return children;
+}
