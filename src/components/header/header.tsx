238f2150dc0677f80ee0954cb484d7564c9cb4ef
Andrei Yurkouski <emp74ark@gmail.com>
2025-04-06 00:59:55 +0200

    style: header change style on scroll

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,38 @@
+'use client';
+
 import { Icon, LocaleSwitcher, Nav, ThemeSwitcher } from '@/components';
 import styles from './header.module.scss';
+import { useEffect, useRef, useState } from 'react';
+import clsx from 'clsx';
 
 export function Header() {
+  const headerRef = useRef<HTMLDivElement>(null);
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!headerRef.current) return;
+      const { offsetHeight } = headerRef.current;
+
+      if (window.scrollY > offsetHeight) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [headerRef]);
+
   return (
-    <header className={styles.header}>
+    <header
+      className={clsx(styles.header, isScrolled && styles.header_scrolled)}
+      ref={headerRef}
+    >
       <Icon iconName="planet" size="2rem" />
       <div className={styles.buttons}>
         <ThemeSwitcher />
