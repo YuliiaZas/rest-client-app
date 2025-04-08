c6fd50e22432e09c2d43af5086f24d68ad50fcc9
Andrei Yurkouski <emp74ark@gmail.com>
2025-04-08 14:31:04 +0200

    test: dropdown component tests

---
+++ src/components/dropdown/index.test.tsx
@@ -0,0 +1,75 @@
+import { cleanup, render, screen } from '@testing-library/react';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { Dropdown } from './index';
+
+const itemsMock = [
+  { value: 'item1', label: 'Item 1' },
+  { value: 'item2', label: 'Item 2' },
+];
+
+describe('Dropdown', () => {
+  let selectOptionMock: () => void;
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+    cleanup();
+    selectOptionMock = vi.fn();
+  });
+
+  it('renders button with correct text', () => {
+    render(
+      <Dropdown
+        items={itemsMock}
+        selectedItem="item1"
+        selectOption={selectOptionMock}
+        buttonChildren="Select an item"
+      />
+    );
+    expect(screen.getByText('Select an item')).toBeDefined();
+  });
+
+  it('opens dropdown when button is clicked', async () => {
+    render(
+      <Dropdown
+        items={itemsMock}
+        selectedItem="item1"
+        selectOption={selectOptionMock}
+      />
+    );
+    const button = screen.getByRole('button');
+    await button.click();
+    expect(screen.getAllByText('Item 1').length).toBe(2);
+    expect(screen.getByText('Item 2')).toBeDefined();
+  });
+
+  it('closes dropdown when clicking on the dropdown', async () => {
+    render(
+      <Dropdown
+        items={itemsMock}
+        selectedItem="item1"
+        selectOption={selectOptionMock}
+      />
+    );
+    const button = screen.getByRole('button');
+    await button.click();
+    expect(screen.getAllByText(/Item/).length).toBe(3);
+    await button.click();
+    expect(screen.getAllByText(/Item/).length).toBe(1);
+  });
+
+  it('calls selectOption when an item is selected', async () => {
+    render(
+      <Dropdown
+        items={itemsMock}
+        selectedItem="item1"
+        selectOption={selectOptionMock}
+      />
+    );
+    const button = screen.getByRole('button');
+    await button.click();
+    const item = screen.getByText('Item 2');
+    expect(item).toBeDefined();
+    await item.click();
+    expect(selectOptionMock).toHaveBeenCalledWith('item2');
+  });
+});
