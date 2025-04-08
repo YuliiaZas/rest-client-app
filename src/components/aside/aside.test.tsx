479d4cdcda6e7e1bf21f7a5244794ac84afb27b3
Andrei Yurkouski <emp74ark@gmail.com>
2025-04-08 13:39:05 +0200

    test: aside component tests

---
+++ src/components/aside/aside.test.tsx
@@ -0,0 +1,30 @@
+import { cleanup, render, screen } from '@testing-library/react';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { Aside } from './aside';
+
+describe('Aside Component', () => {
+  beforeEach(() => {
+    cleanup();
+  });
+
+  it('should render children correctly', () => {
+    render(<Aside type="root">Root</Aside>);
+    expect(screen.getByText('Root')).toBeDefined();
+  });
+
+  it('should apply the correct class for the root type', () => {
+    render(<Aside type="root">Root</Aside>);
+    const asideElement = screen.getByText('Root');
+    console.log(asideElement.className);
+    expect(asideElement).toBeDefined();
+    expect(asideElement.className.includes(`aside__root`)).toBeTruthy();
+  });
+
+  it('should apply the correct class for the client type', () => {
+    render(<Aside type="client">Client</Aside>);
+    const asideElement = screen.getByText('Client');
+    console.log(asideElement.className);
+    expect(asideElement).toBeDefined();
+    expect(asideElement.className.includes(`aside__client`)).toBeTruthy();
+  });
+});
