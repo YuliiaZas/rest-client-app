3e24fec4af2759d76cb5865936d6bddc577826dd
Andrei Yurkouski <emp74ark@gmail.com>
2025-04-12 00:02:10 +0200

    test: add tests for RootLoading component

---
+++ src/__tests__/loading.test.tsx
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import RootLoading from '../app/loading';
+
+describe('Root loading', () => {
+  it('renders the default spinner', () => {
+    render(<RootLoading />);
+    const spinner = screen.getByRole('status');
+    expect(spinner.className.includes('spinner')).toBeTruthy();
+  });
+});
