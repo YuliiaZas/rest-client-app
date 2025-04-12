c1638c0855bfc6d69051e2c909c8afab73a2b0fb
Andrei Yurkouski <emp74ark@gmail.com>
2025-04-12 23:13:52 +0200

    test: tests for the ThemeSwitcher component

---
+++ src/components/theme-switcher/theme-switcher.test.tsx
@@ -0,0 +1,52 @@
+import { render, screen } from '@testing-library/react';
+import { afterEach, describe, expect, it, vi } from 'vitest';
+import { ThemeSwitcher } from '@/components';
+import { useTheme } from '@/hooks';
+import { AppTheme } from '@/entites';
+
+vi.mock('@/hooks', () => ({
+  useTheme: vi.fn().mockReturnValue({
+    theme: 'light',
+    setTheme: vi.fn(),
+  }),
+}));
+
+describe('ThemeSwitcher', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('renders button with correct title', () => {
+    render(<ThemeSwitcher />);
+    expect(screen.getByRole('button', { name: 'theme' })).toBeDefined();
+  });
+
+  it('toggles theme from light to dark on button click', async () => {
+    const setThemeMock = vi.fn();
+
+    vi.mocked(useTheme).mockReturnValue({
+      theme: AppTheme.light,
+      setTheme: setThemeMock,
+    });
+
+    render(<ThemeSwitcher />);
+    const button = screen.getByRole('button', { name: 'theme' });
+    button.click();
+    expect(setThemeMock).toHaveBeenCalledWith(AppTheme.dark);
+  });
+
+  it('toggles theme from dark to light on button click', async () => {
+    const setThemeMock = vi.fn();
+
+    vi.mocked(useTheme).mockReturnValue({
+      theme: AppTheme.dark,
+      setTheme: setThemeMock,
+    });
+
+    render(<ThemeSwitcher />);
+    const button = screen.getByRole('button', { name: 'theme' });
+    button.click();
+
+    expect(setThemeMock).toHaveBeenCalledWith(AppTheme.light);
+  });
+});
